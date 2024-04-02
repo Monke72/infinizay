7080d35f383ea5396163655852302e885f0acd92
Artem <wera16307@gmail.com>
2024-04-02 15:45:51 +0400

    new file for project

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ const socialsList = document.querySelector(".header__main-vert__list");
 socialsBtn.addEventListener("click", () => {
   socialsList.classList.toggle("header--list--none");
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
